cbf59efb473b2f82f2fb06a432201178c1488b41
malaika22 <malaikaafridi22@gmail.com>
2025-12-13 20:58:18 +0500

    added auth layout

---
+++ src/components/auth/AuthHeroSection.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image';
+
+export const AuthHeroSection = () => {
+  return (
+    <div className="flex-1 bg-background-primary-light flex flex-col items-center relative pt-20 gap-20 px-3">
+      <div>
+        <div className="text-[40px] text-accent-foreground font-semibold mb-4">
+          Huumo
+        </div>
+        <div className="text-base text-secondary-foreground">
+          <div>
+            <span className="font-semibold">Automate</span> the chase.{' '}
+            <span className="font-semibold">Accelerate</span> your POs.{' '}
+          </div>
+          <div>
+            <span className="font-semibold">AI-driven follow-ups</span> that
+            keep your supply chain moving without delays.
+          </div>
+        </div>
+      </div>
+      <div>
+        <Image
+          src="/images/auth/auth-image.svg"
+          width={544}
+          height={544}
+          alt="auth-image"
+        />
+      </div>
+    </div>
+  );
+};
