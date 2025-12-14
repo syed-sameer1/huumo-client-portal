f37744348db12e0152d173b9b994c517803ee2c6
malaika22 <malaikaafridi22@gmail.com>
2025-12-14 13:40:21 +0500

    create auth schema for login + signup

---
+++ src/schema/authSchema.ts
@@ -0,0 +1,24 @@
+import * as z from 'zod';
+
+export const loginSchema = z.object({
+  email: z.email({ message: 'Invalid email address' }),
+  password: z
+    .string()
+    .min(8, { message: 'Password must be atleast 8 characters' }),
+});
+
+export const signupSchema = z
+  .object({
+    name: z.string().min(2, { message: 'Name must be at least 2 characters' }),
+    email: z.email({ message: 'Invalid email address' }),
+    password: z
+      .string()
+      .min(8, { message: 'Password must be at least 8 characters' }),
+    confirmPassword: z
+      .string()
+      .min(8, { message: 'Please confirm your password' }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ['confirmPassword'],
+  });
