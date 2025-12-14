0ce8c2736f603e8a1a9d974693c2f0a08016f783
malaika22 <malaikaafridi22@gmail.com>
2025-12-14 13:40:38 +0500

    added themeing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,16 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
+  content: [
+    './app/**/*.{ts,tsx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+  ],
   theme: {
     extend: {
+      boxShadow: {
+        soft: '0px 1px 2px 0px #0000000F, 0px 1px 3px 0px #0000001A',
+      },
       colors: {
         background: {
           secondary: {
@@ -12,6 +20,9 @@ const config: Config = {
         accent: {
           DEFAULT: 'hsl(var(--accent))',
           foreground: 'hsl(var(--accent-foreground))',
+        },
+        foreground: {
+          DEFAULT: 'var(--foreground-default)',
         },
       },
     },
