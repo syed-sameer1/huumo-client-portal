3a6d87a78d78fea99608a6cb6beeb2cf44828c80
malaika22 <malaikaafridi22@gmail.com>
2026-01-04 13:22:48 +0500

    added success + onboarding

---
+++ src/app/onboarding/page.tsx
@@ -0,0 +1,20 @@
+import { ProfileDropdown } from '@/components/layout/Header/ProfileDropdown';
+import { Onboarding } from '@/components/onboarding';
+import Image from 'next/image';
+
+export default function OnboardingPage() {
+  return (
+    <div>
+      <div className="flex items-center py-4 px-8 h-20.5 bg-background-primary-light border-b justify-between">
+        <Image
+          alt="huumo"
+          src="/images/subscription/huumo-subscription.svg"
+          width={166}
+          height={50}
+        />
+        <ProfileDropdown />
+      </div>
+      <Onboarding />
+    </div>
+  );
+}
