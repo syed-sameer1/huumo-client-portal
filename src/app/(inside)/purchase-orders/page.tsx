e0690436237e3671434c8c9a17908558afc57930
malaika22 <malaikaafridi22@gmail.com>
2026-01-04 13:22:21 +0500

    updated purchase orders routing

--- src/app/(inside)/purchase-orders/page.tsx
+++ src/app/(inside)/purchase-orders/page.tsx
@@ -1,16 +1,5 @@
-import { PageContainer } from '@/components/layout/PageContainer';
-import { Title } from '@/components/layout/Title';
-import { ActionButtons } from '@/components/purchase-orders/ActionButtons';
-import { NoPurchaseOrder } from '@/components/purchase-orders/NoPurchaseOrder';
+import { PurchaseOrders } from '@/components/purchase-orders';
 
 export default function PurchaseOrdersPage() {
-  return (
-    <PageContainer>
-      <div className="flex justify-between">
-        <Title>Purchase Orders</Title>
-        <ActionButtons />
-      </div>
-      <NoPurchaseOrder />
-    </PageContainer>
-  );
+  return <PurchaseOrders />;
 }
