7cb89bb93bfd2e365fd84548450aeab22c23aeaa
malaika22 <malaikaafridi22@gmail.com>
2026-01-05 23:33:01 +0500

    added column mapping breadcrumb

---
+++ src/constants/breadcrumbs.ts
@@ -0,0 +1,33 @@
+export const SelectImportMethodBreadcrumbs = [
+  {
+    href: 'purchase-orders',
+    label: 'Purchase Orders',
+  },
+  {
+    href: 'new-purchase-order',
+    label: 'New PO',
+  },
+  {
+    href: 'select-import-method',
+    label: 'Select Import Method',
+  },
+];
+
+export const ColumnMappingBreadcrumbs = [
+  {
+    href: 'purchase-orders',
+    label: 'Purchase Orders',
+  },
+  {
+    href: 'new-purchase-order',
+    label: 'New PO',
+  },
+  {
+    href: 'select-import-method',
+    label: 'Select Import Method',
+  },
+  {
+    href: 'column-mapping',
+    label: 'Column Mapping',
+  },
+];
