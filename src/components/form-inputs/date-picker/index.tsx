6b952753e4edfd4aa375a4d36004cc7c488086df
malaika22 <malaikaafridi22@gmail.com>
2026-01-18 23:33:53 +0500

    added date picker component

---
+++ src/components/form-inputs/date-picker/index.tsx
@@ -0,0 +1,97 @@
+'use client';
+
+import * as React from 'react';
+import { CalendarDaysIcon } from 'lucide-react';
+
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+import { Input } from '@/components/ui/input';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+
+function formatDate(date: Date | undefined) {
+  if (!date) {
+    return '';
+  }
+
+  return date.toLocaleDateString('en-US', {
+    day: '2-digit',
+    month: 'long',
+    year: 'numeric',
+  });
+}
+
+function isValidDate(date: Date | undefined) {
+  if (!date) {
+    return false;
+  }
+  return !isNaN(date.getTime());
+}
+
+export function DatePicker() {
+  const [open, setOpen] = React.useState(false);
+  const [date, setDate] = React.useState<Date | undefined>();
+  const [month, setMonth] = React.useState<Date | undefined>(date);
+  const [value, setValue] = React.useState(formatDate(date));
+
+  return (
+    <div className="flex flex-col gap-3">
+      <div className="relative flex gap-2">
+        <Input
+          id="date"
+          value={value}
+          placeholder="Filter by date"
+          className="bg-background pr-10 w-58.25 text-sm"
+          onChange={(e) => {
+            const date = new Date(e.target.value);
+            setValue(e.target.value);
+            if (isValidDate(date)) {
+              setDate(date);
+              setMonth(date);
+            }
+          }}
+          onKeyDown={(e) => {
+            if (e.key === 'ArrowDown') {
+              e.preventDefault();
+              setOpen(true);
+            }
+          }}
+        />
+        <Popover open={open} onOpenChange={setOpen}>
+          <PopoverTrigger asChild>
+            <Button
+              id="date-picker"
+              variant="ghost"
+              className="absolute top-1/2 right-2 size-6 -translate-y-1/2"
+            >
+              <CalendarDaysIcon className="size-3.5 text-[#71717A]" />
+              <span className="sr-only">Select date</span>
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent
+            className="w-auto overflow-hidden p-0"
+            align="end"
+            alignOffset={-8}
+            sideOffset={10}
+          >
+            <Calendar
+              mode="single"
+              selected={date}
+              captionLayout="dropdown"
+              month={month}
+              onMonthChange={setMonth}
+              onSelect={(date) => {
+                setDate(date);
+                setValue(formatDate(date));
+                setOpen(false);
+              }}
+            />
+          </PopoverContent>
+        </Popover>
+      </div>
+    </div>
+  );
+}
