392cff1e4e96278782bfebddb7137b951ef13313
malaika22 <malaikaafridi22@gmail.com>
2026-04-02 19:57:58 +0500

    added manual purchase order flow

---
+++ src/app/(inside)/purchase-orders/manual-purchase-order/page.tsx
@@ -0,0 +1,10 @@
+import { PageContainer } from '@/components/layout/PageContainer';
+import { ManualPurchaseOrder } from '@/components/purchase-orders/ManualPurchaseOrder';
+
+export default function ManualPurchaseOrderPage() {
+  return (
+    <PageContainer>
+      <ManualPurchaseOrder />
+    </PageContainer>
+  );
+}
